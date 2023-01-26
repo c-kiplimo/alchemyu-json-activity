b61d68f2a3a5291481152de07993922e386d0189
kiplimo-dev <kiplimocollins855@gmail.com>
2023-01-26 10:46:43 +0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+
+const axios = require('axios');
 
 // copy-paste your URL provided in your Alchemy.com dashboard
-const ALCHEMY_URL = "";
+const ALCHEMY_URL = "https://eth-mainnet.g.alchemy.com/v2/f77ma6-G6-pYqsJCeM2rwuVE4HUffddD";
 
 axios.post(ALCHEMY_URL, {
   jsonrpc: "2.0",
